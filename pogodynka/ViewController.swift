57bda77d61158dcfc125fb93bd18ac9f05d40efa
Guest User <Guest@imac-08.local>
2019-10-16 19:25:12 +0200

    Add UI code references

--- pogodynka/ViewController.swift
+++ pogodynka/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var tempMin: UILabel!
+    @IBOutlet weak var tempMax: UILabel!
+    @IBOutlet weak var windVelocity: UILabel!
+    @IBOutlet weak var windDirection: UILabel!
+    @IBOutlet weak var rain: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBAction func previousDay(_ sender: Any) {
+    }
+    @IBAction func nextDay(_ sender: Any) {
+    }
+    
+    // class var shared: URLSession { get { return URLP{} } }
+    // https://www.metaweather.com/api/location/44418/
 
 }
 
