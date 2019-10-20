ed4c8b59242d2d3761b28a126a220671072b0bcf
Mateusz Grzelinski <czubaka111@gmail.com>
2019-10-20 22:04:17 +0200

    Add weather stucture

---
+++ pogodynka/Pogoda.swift
@@ -0,0 +1,62 @@
+
+import Foundation
+import CoreLocation
+
+struct Pogoda {
+  let weatherType:String
+  let tempMax:Double
+  let tempMin:Double
+  let windVelocity:Double
+  let windDirection:Double
+  let rain:Double
+  let pressure:Double
+  let icon:String
+  let timeStamp:Double
+
+  init(json:[String:Any]) {
+    self.weatherType = json["summary"] as! String
+    self.tempMax = json["temperatureMax"] as! Double
+    self.tempMin = json["temperatureMin"] as! Double
+    self.windVelocity = json["windSpeed"] as! Double
+    self.windDirection = json["windBearing"] as! Double
+    self.rain = json["humidity"] as! Double
+    self.pressure = json["pressure"] as! Double
+    self.icon = json["icon"] as! String
+    self.timeStamp = json["time"] as! Double
+  }
+
+
+  static func getData (latitude: String, longitude: String, completion: @escaping ([Pogoda]?) -> ()) {
+
+    let API_PATH = "https://api.darksky.net/forecast/23a47f99989a11f4a6a3494dd8c5cbfa/"
+
+    let url = API_PATH + "\(latitude),\(longitude)?exclude=currently,minutely,hourly&units=si"
+    let request = URLRequest(url: URL(string: url)!)
+
+    let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+      var forecastArray:[Pogoda] = []
+      if let data = data {
+        do {
+          if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+            if let dailyForecasts = json["daily"] as? [String:Any] {
+              if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
+                for dataPoint in dailyData {
+                  if let weatherObject = try? Pogoda(json: dataPoint) {
+                    forecastArray.append(weatherObject)
+                  }
+                }
+              }
+            }
+
+          }
+        }catch {
+          print(error.localizedDescription)
+        }
+        completion(forecastArray)
+      }
+    }
+    task.resume()
+  }
+
+
+}
--- pogodynka/ViewController.swift
+++ pogodynka/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var weatherData: [Pogoda]()
+  var i: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        Pogoda.getData(latitude: "19.9872", longitude: "50.0527", completion:{ (results:[Weather]?) in
+            if let weatherData = results {
+                self.weatherData = weatherData
+                
+                DispatchQueue.main.async {
+                    self.reloadWeather(i: self.i)
+                }
+                
+            }
+        })
     }
 
     @IBOutlet weak var weatherIcon: UIImageView!
@@ -23,9 +36,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var rain: UILabel!
     @IBOutlet weak var pressure: UILabel!
     @IBAction func previousDay(_ sender: Any) {
+       if(self.i < self.weatherData.count - 1) {
+            self.i += 1
+            self.reloadWeather(i: self.i)
+        }
     }
     @IBAction func nextDay(_ sender: Any) {
+       if(self.i > 0) {
+            self.i -= 1
+            self.reloadWeather(i: self.i)
+        }
     }
+
+    func reloadWeather(i: Int) {
+        self.displayDate(unixtimeInterval: self.weatherData[i].timeStamp)
+        self.weatherIcon.image = UIImage(named: self.weatherData[i].weatherIcon)
+        self.tempMin.text = String(self.weatherData[i].tempMin)
+        self.tempMax.text = String(self.weatherData[i].tempMax)
+        self.windVelocity.text = String(self.weatherData[i].windVelocity)
+        self.windDirection.text = String(self.weatherData[i].windDirection)
+        self.rain.text = String(self.weatherData[i].rain)
+        self.pressure.text = String(self.weatherData[i].pressure)
+    }
+
     
     // class var shared: URLSession { get { return URLP{} } }
     // https://www.metaweather.com/api/location/44418/
