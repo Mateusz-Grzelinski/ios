d8907a7158cb38e21da9372c92701b3d4ee8f415
Guest User <Guest@imac-08.local>
2019-10-23 19:27:14 +0200

    lab2

--- pogodynka/Pogoda.swift
+++ pogodynka/Pogoda.swift
@@ -3,59 +3,57 @@ import Foundation
 import CoreLocation
 
 struct Pogoda {
-  let weatherType:String
-  let tempMax:Double
-  let tempMin:Double
-  let windVelocity:Double
-  let windDirection:Double
-  let rain:Double
-  let pressure:Double
-  let icon:String
-  let timeStamp:Double
-
-  init(json:[String:Any]) {
-    self.weatherType = json["summary"] as! String
-    self.tempMax = json["temperatureMax"] as! Double
-    self.tempMin = json["temperatureMin"] as! Double
-    self.windVelocity = json["windSpeed"] as! Double
-    self.windDirection = json["windBearing"] as! Double
-    self.rain = json["humidity"] as! Double
-    self.pressure = json["pressure"] as! Double
-    self.icon = json["icon"] as! String
-    self.timeStamp = json["time"] as! Double
-  }
-
-
-  static func getData (latitude: String, longitude: String, completion: @escaping ([Pogoda]?) -> ()) {
-
-    let API_PATH = "https://api.darksky.net/forecast/13066f8bc7d489b2e2f550c8237de8c4/" + "\(latitude),\(longitude)i"
-    let request = URLRequest(url: URL(string: url)!)
-
-    let task = URLSession.shared.dataTask(with: request) 
-    { (data:Data?, response:URLResponse?, error:Error?) in
-      var forecastArray:[Pogoda] = []
-      if let data = data {
-        do {
-          if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
-            if let dailyForecasts = json["daily"] as? [String:Any] {
-              if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
-                for dataPoint in dailyData {
-                  if let weatherObject = try? Pogoda(json: dataPoint) {
-                    forecastArray.append(weatherObject)
-                  }
-                }
-              }
-            }
-
-          }
-        }catch {
-          print(error.localizedDescription)
-        }
-        completion(forecastArray)
-      }
+    let weatherType:String
+    let tempMax:Double
+    let tempMin:Double
+    let windVelocity:Double
+    let windDirection:Double
+    let rain:Double
+    let pressure:Double
+    let iconName:String
+    let timeStamp:Double
+    
+    init(json:[String:Any]) {
+        self.weatherType = json["summary"] as! String
+        self.tempMax = json["temperatureMax"] as! Double
+        self.tempMin = json["temperatureMin"] as! Double
+        self.windVelocity = json["windSpeed"] as! Double
+        self.windDirection = json["windBearing"] as! Double
+        self.rain = json["humidity"] as! Double
+        self.pressure = json["pressure"] as! Double
+        self.iconName = json["icon"] as! String
+        self.timeStamp = json["time"] as! Double
     }
-    task.resume()
-  }
-
-
+    
+    
+    static func getData (latitude: String, longitude: String, completion: @escaping ([Pogoda]?) -> ()) {
+        
+        let API_PATH = "https://api.darksky.net/forecast/13066f8bc7d489b2e2f550c8237de8c4/" + "\(latitude),\(longitude)i"
+        let request = URLRequest(url: URL(string: API_PATH)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+            var forecastArray:[Pogoda] = []
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        if let dailyForecasts = json["daily"] as? [String:Any] {
+                            if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
+                                for dataPoint in dailyData {
+                                    if let weatherObject = try? Pogoda(json: dataPoint) {
+                                        forecastArray.append(weatherObject)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+                completion(forecastArray)
+            }
+        }
+        task.resume()
+        
+    }
 }
+
--- pogodynka/ViewController.swift
+++ pogodynka/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-  var weatherData: [Pogoda]()
+  var weatherData: [Pogoda] = []
   var i: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        Pogoda.getData(latitude: "19.9872", longitude: "50.0527", completion:{ (results:[Weather]?) in
+        Pogoda.getData(latitude: "19.9872", longitude: "50.0527", completion:{ (results:[Pogoda]?) in
             if let weatherData = results {
                 self.weatherData = weatherData
                 
@@ -28,13 +28,21 @@ class ViewController: UIViewController {
         })
     }
 
+    @IBOutlet weak var iconName: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
+    //    @IBOutlet weak var tempMin: UILabel!
+//    @IBOutlet weak var tempMax: UILabel!
+//    @IBOutlet weak var windVelocity: UILabel!
+//    @IBOutlet weak var windDirection: UILabel!
+//    @IBOutlet weak var rain: UILabel!
+//    @IBOutlet weak var pressure: UILabel!
     @IBOutlet weak var tempMin: UILabel!
     @IBOutlet weak var tempMax: UILabel!
     @IBOutlet weak var windVelocity: UILabel!
     @IBOutlet weak var windDirection: UILabel!
     @IBOutlet weak var rain: UILabel!
     @IBOutlet weak var pressure: UILabel!
+    
     @IBAction func previousDay(_ sender: Any) {
        if(self.i < self.weatherData.count - 1) {
             self.i += 1
@@ -49,8 +57,9 @@ class ViewController: UIViewController {
     }
 
     func reloadWeather(i: Int) {
-        self.displayDate(unixtimeInterval: self.weatherData[i].timeStamp)
-        self.weatherIcon.image = UIImage(named: self.weatherData[i].weatherIcon)
+        //self.displayDate(unixtimeInterval: self.weatherData[i].timeStamp)
+        //self.weatherIcon.image = UIImage(named: self.weatherData[i].weatherIcon)
+        self.iconName.text = String(self.weatherData[i].iconName)
         self.tempMin.text = String(self.weatherData[i].tempMin)
         self.tempMax.text = String(self.weatherData[i].tempMax)
         self.windVelocity.text = String(self.weatherData[i].windVelocity)
