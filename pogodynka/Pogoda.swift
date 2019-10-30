773b5dab1789560dbb9fc16f3eac55dd85c01a59
Mateusz-gmail <mgrzel@student.agh.edu.pl>
2019-10-30 15:03:26 +0100

    Finish lab 1

--- pogodynka/Pogoda.swift
+++ pogodynka/Pogoda.swift
@@ -28,25 +28,29 @@ struct Pogoda {
     
     static func getData (latitude: String, longitude: String, completion: @escaping ([Pogoda]?) -> ()) {
         
-        let API_PATH = "https://api.darksky.net/forecast/13066f8bc7d489b2e2f550c8237de8c4/" + "\(latitude),\(longitude)i"
+        let API_PATH = "https://api.darksky.net/forecast/13066f8bc7d489b2e2f550c8237de8c4/\(latitude),\(longitude)"
         let request = URLRequest(url: URL(string: API_PATH)!)
         
         let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
-            var forecastArray:[Pogoda] = []
-            if let data = data {
-                do {
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
-                        if let dailyForecasts = json["daily"] as? [String:Any] {
-                            if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
-                                for dataPoint in dailyData {
-                                    if let weatherObject = try? Pogoda(json: dataPoint) {
-                                        forecastArray.append(weatherObject)
-                                    }
+        
+        var forecastArray:[Pogoda] = []
+        
+        if let data = data {
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                    if let dailyForecasts = json["daily"] as? [String:Any] {
+                        if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
+                            for dataPoint in dailyData {
+                                if let weatherObject = try? Pogoda(json: dataPoint) {
+                                    forecastArray.append(weatherObject)
                                 }
                             }
                         }
                     }
-                }catch {
+                
+                }
+            }catch {
                     print(error.localizedDescription)
                 }
                 completion(forecastArray)
--- pogodynka/ViewController.swift
+++ pogodynka/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
         Pogoda.getData(latitude: "19.9872", longitude: "50.0527", completion:{ (results:[Pogoda]?) in
             if let weatherData = results {
                 self.weatherData = weatherData
-                
                 DispatchQueue.main.async {
                     self.reloadWeather(i: self.i)
                 }
@@ -30,12 +29,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var iconName: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
-    //    @IBOutlet weak var tempMin: UILabel!
-//    @IBOutlet weak var tempMax: UILabel!
-//    @IBOutlet weak var windVelocity: UILabel!
-//    @IBOutlet weak var windDirection: UILabel!
-//    @IBOutlet weak var rain: UILabel!
-//    @IBOutlet weak var pressure: UILabel!
     @IBOutlet weak var tempMin: UILabel!
     @IBOutlet weak var tempMax: UILabel!
     @IBOutlet weak var windVelocity: UILabel!
@@ -57,8 +50,8 @@ class ViewController: UIViewController {
     }
 
     func reloadWeather(i: Int) {
-        //self.displayDate(unixtimeInterval: self.weatherData[i].timeStamp)
-        //self.weatherIcon.image = UIImage(named: self.weatherData[i].weatherIcon)
+        //self.??.text = self.formatDate(self.weatherData[i].timeStamp)
+        self.weatherIcon.image = UIImage(named: self.weatherData[i].iconName)
         self.iconName.text = String(self.weatherData[i].iconName)
         self.tempMin.text = String(self.weatherData[i].tempMin)
         self.tempMax.text = String(self.weatherData[i].tempMax)
@@ -67,10 +60,13 @@ class ViewController: UIViewController {
         self.rain.text = String(self.weatherData[i].rain)
         self.pressure.text = String(self.weatherData[i].pressure)
     }
-
     
-    // class var shared: URLSession { get { return URLP{} } }
-    // https://www.metaweather.com/api/location/44418/
+    func formatDate(unixtimeInterval: Double) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        return dateFormatter.string(from: Date(timeIntervalSince1970: unixtimeInterval))
+    }
 
 }
 
